38e5580abc4678a54b0dbe96b996e2bd4d5d8859
Thanawat080 <56470827+Thanawat080@users.noreply.github.com>
2022-06-09 13:05:54 +0700

    Update api.py

--- random/api.py
+++ random/api.py
@@ -10,11 +10,6 @@ def index():
     user_obj = generate()
     return user_obj 
 
-@app.route("/face")
-def face():
-    api = {'api_key':'Wv78mjcR4HC5VWin-YsA2g'}
-    x = requests.get('https://api.generated.photos/api/v1/faces',params=api)
-    return x.text
 
 
 if __name__ == "__main__":
